c77efbdd4ed97223f6e63057b8eb189196c8d09c
Eduardo Lucas <eduardo@ilef.co.uk>
2021-07-01 20:42:43 +0100

    moved our directory and shop data into its own reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -11,7 +11,6 @@ const CollectionPreview = ({ title, items }) => (
       {items
         .filter((item, idx) => idx < 4)
         .map((item) => (
-          // @ts-ignore
           <CollectionItem key={item.id} item={item} />
         ))}
     </div>
